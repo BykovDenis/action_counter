805603588faad48f7bc963d40f04b67e0c83fef8
Bykov Denis <stivenrain@gmail.com>
2015-11-16 23:39:35 +0300

    design counter

---
+++ index.php
@@ -0,0 +1,62 @@
+<html lang="ru">
+    <head>
+       <meta charset="utf-8">
+        <title>Счетчик для акции</title>
+        <link href="css/normalize.css" rel="stylesheet"></link>
+        <link href="css/style.css" rel="stylesheet"></link>
+    </head>
+    <body>
+        <article class="counter">
+            <h2 class="counter__header clearfix">
+                До конца акции осталось:
+            </h2>        
+            <div class="counter__content clearfix">    
+                <div class="date clearfix">
+                   <div class="date__layout">
+                        <div class="date__day">
+                            35
+                       <div class="date__border">
+                       </div>                            
+                        </div> 
+
+                    </div>
+                    <p>дней</p>
+                </div>
+                <div class="time">
+                    <ul>
+                    <li>
+                       <div class="time__layout">
+                            <div class="time__item">
+                            24
+                           <div class="time__border">
+                           </div>   
+                            </div>
+                        </div>
+                        <p>часов</p>
+                    </li>
+                    <li>
+                       <div class="time__layout">
+                            <div class="time__item">
+                                19
+                                <div class="time__border">
+                                </div>   
+                            </div>
+                        </div>
+                            <p>минут</p>
+                    </li>
+                    <li>
+                        <div class="time__layout">
+                            <div class="time__item">
+                            56
+                            <div class="time__border">
+                            </div>                               
+                            </div> 
+                        </div>
+                        <p>секунд</p>
+                    </li>
+                    </ul>
+                </div>
+            </div>
+        </article>
+    </body>
+</html>
